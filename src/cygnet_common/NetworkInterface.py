38d59f2d71c5d5f31ab37cf02403e0521299835b
Saad Talaat <saad@cygnus.email>
2015-09-03 09:48:22 +0300

    [NetworkInterfaces] Cleaning old methods
    [Interfaces] Changing OVSInterface to inherit dict, using pyroute2 to set addrs

--- src/cygnet_common/NetworkInterface.py
+++ src/cygnet_common/NetworkInterface.py
@@ -1,4 +1,3 @@
-
 from interfaces import OVSInterface
 
 class NetworkInterface(dict):
@@ -84,29 +83,3 @@ class NetworkInterface(dict):
             print "NetworkInterface: Cannot disconnect container, container isn't connected in the first place"
             return
         del self.containers[self.containers.index(container)]
-
-    ##### Type oriented methods ######
-    def __keytransform__(self, key):
-        key = key.lower()
-        allowed_keys = ["interfaces","endpoints","containers"]
-        try:
-            index = [n.find(key) for n in allowed_keys].index(0)
-        except ValueError as e:
-            index = -1
-
-        if index >= 0 and len(key) == len(allowed_keys[index]):
-            return key
-        else:
-            raise NameError("NetworkInterface: Illegal key")
-
-    @property
-    def interfaces(self):
-        return self['interfaces']
-
-    @property
-    def endpoints(self):
-        return self['endpoints']
-
-    @property
-    def containers(self):
-        return self['containers']
--- src/cygnet_common/interfaces.py
+++ src/cygnet_common/interfaces.py
@@ -1,22 +1,50 @@
 
-from pyroute2 import IPDB
+from pyroute2 import IPRoute, IPDB
 from sarge import run
 
-class OVSInterface(object):
+class OVSInterface(dict):
     '''
         Use an OVS client to query the database for current ovs network.
     '''
-    def __init__(self, interface):
+    def __init__(self, interface, **kwargs):
+        dict.__init__(self,kwargs)
         self.addr = None
-        self.endpoints = []
         self.range_buckets = {}
         self.interface = interface
         for i in range(1,255):
             self.range_buckets[i] = None
-
+        self['endpoints'] = kwargs['endpoints']
+        self['containers'] = kwargs['containers']
+        self['interfaces'] = kwargs['interfaces']
         ####
         # Should read database here
         ####
+
+    def __getattribute__(self, key, *args):
+        try:
+            return dict.__getattribute__(self, key)
+        except AttributeError as e:
+            if key in self:
+                return self[key]
+            else:
+                raise e
+
+    def __setattr__(self, key, value):
+        try:
+            dict.__setattr__(self, key, value)
+        except AttributeError as e:
+            if key in self:
+                self[key] = value
+            else:
+                raise e
+    def __delattr__(self, key):
+        try:
+            dict.__delattr__(self, key)
+        except AttributeError as e:
+            if key in self:
+                del self[key]
+            else:
+                raise e
 
     def initalize(self):
         ip = IPDB()
@@ -25,7 +53,7 @@ class OVSInterface(object):
             self.addr = list(ip.interfaces['br1']['ipaddr'])[0]
             self.addr = self.addr[0], str(self.addr[1])
             self.interface.interfaces.append(('br1',self.addr))
-
+            self.interfaces.append(('br1',self.addr))
         except Exception as e:
             print e
         finally:
@@ -35,18 +63,25 @@ class OVSInterface(object):
         return addr
 
     def initContainerNetwork(self, count):
-        mask = "16"
+        ip = IPRoute()
+        mask = 16
         addr = "10.1."+str(count)+".1"
-        addr2 = addr +"/"+ mask
         run("ifconfig br2 "+addr2)
+        ip.addr('add',
+                index=(ip.link_lookup(ifname='br2')),
+                address='addr',
+                mask=mask)
         self.interface.interfaces.append(('br2',(self.addr,mask)))
+        self.interfaces.append(('br2',(self.addr,str(mask))))
         return addr
 
     def addEndpoint(self, endpoint):
         run("./cmds/establish-gre.sh" + str(endpoint[1]) + " " + endpoint[2])
+        self.endpoints.append(endpoint)
 
     def removeEndpoint(self, endpoint):
         run("ovs-vsctl del-port gre"+str(endpoint[1]))
+        del self.endpoints[self.endpoints.index(endpoint)]
 
     def connectContainer(self, container):
         addr = str(container["Address"])
@@ -58,8 +93,10 @@ class OVSInterface(object):
             run("pipework br2 -i eth1 "+ containerId + " " + addr)
         else:
             print "Error connecting container",containerId+": Address Already taken by container: ",self.range_buckets[addr_idx]
+        self.containers.append(container)
 
     def disconnectContainer(self, container):
         addr = str(container["Address"])
         addr_idx = int(addr.split("/")[0].split(".")[-1])
         self.range_buckets[addr_idx]= None
+        del self.containers[self.containers.index(container)]
