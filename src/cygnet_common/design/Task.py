f0620676b18c56bc216904a9dc5de91e1b4b9f6b
Saad Talaat <saad@cygnus.email>
2015-09-17 00:31:06 +0300

    Adding design directory

---
+++ src/cygnet_common/design/Task.py
@@ -0,0 +1,20 @@
+import threading
+import time
+
+class TaskInterval(threading.Thread):
+    def __init__(self, interval, task, args=None):
+        self.task = task
+        self.interval = interval
+        self.args = args
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        print "[Task Interval] Registered"
+
+    def run(self):
+        print "[Task Interval] Running"
+        while True:
+            time.sleep(self.interval)
+            if self.args:
+                apply(self.task, self.args)
+            else:
+                apply(self.task)
